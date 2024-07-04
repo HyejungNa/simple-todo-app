bd6903315b84d0883c4f34b4e9f7f2f12739cf84
Hyejung Na <hyejung.na01@gmail.com>
2024-07-05 02:57:13 +1000

    Updated logic for filter function

--- main.js
+++ main.js
@@ -6,9 +6,9 @@ let mode = 'all';
 let filterList = [];
 
 addButton.addEventListener('click', addTask);
-taskInput.addEventListener('keypress', function (event) {
+taskInput.addEventListener('keydown', function (event) {
   if (event.key === 'Enter') {
-    addTask();
+    addTask(event);
   }
 });
 // 인풋창에 enter입력시 아이템 추가하기
@@ -71,28 +71,48 @@ function render() {
 }
 
 function toggleComplete(id) {
-  console.log('id:', id);
+  // console.log('id:', id);
   for (let i = 0; i < taskList.length; i++) {
     if (taskList[i].id == id) {
-      // taskList[i].isComplete = true; // 변경전 : isComplete를 항상 true로 설정하면, 이미 완료된 할 일을 다시 미완료 상태로 변경할 수 없음
+      // taskList[i].isComplete = true;
+      // 변경전 : isComplete를 항상 true로 설정하면, 이미 완료된 할 일을 다시 미완료 상태로 변경할 수 없음
       // 변경후 : isComplete 값을 반전시키도록 변경함으로써 클릭할 때마다 완료 상태와 미완료 상태를 전환가능
-      taskList[i].isComplete = !taskList[i].isComplete;
+      taskList[i].isComplete = !taskList[i].isComplete; // 작업의 완료 상태를 반전시킴
+      updateFilterList(taskList[i]); // 해당 작업을 기준으로 filterList 업데이트
       break;
     }
   }
   render();
-  console.log(taskList);
+  // console.log(taskList);
+}
+
+function updateFilterList(task) {
+  let indexInFilter = filterList.findIndex((item) => item.id === task.id);
+
+  if (indexInFilter !== -1) {
+    filterList[indexInFilter].isComplete = task.isComplete;
+  } else {
+    filterList.push(task);
+  }
+
+  if (task.isComplete && mode === 'ongoing') {
+    // '진행 중' 탭에서 완료된 항목을 제거
+    filterList = filterList.filter((item) => !item.isComplete);
+  } else if (!task.isComplete && mode === 'done') {
+    // '완료' 탭에서 미완료된 항목을 제거
+    filterList = filterList.filter((item) => item.isComplete);
+  }
 }
 
 function deleteTask(id) {
-  // deleteTask() 함수가 taskList에서만 삭제하고있어 모든 탭에서 작업이 삭제되지 않음
+  // deleteTask() 함수가 taskList에서만 삭제하고있어 All탭에서만 삭제되고 있음
   for (let i = 0; i < taskList.length; i++) {
     if (taskList[i].id === id) {
       taskList.splice(i, 1);
       break;
     }
   }
-  // deleteTask() 함수를 filterList에서도 사용하여 삭제하면 다른탭에서도 삭제가 가능
+  // deleteTask() 함수를 filterList에서도 사용하면 다른탭에서도 삭제 가능
   for (let i = 0; i < filterList.length; i++) {
     if (filterList[i].id === id) {
       filterList.splice(i, 1);
